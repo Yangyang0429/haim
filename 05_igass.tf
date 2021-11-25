3e3c82e9c5f5b1456250bbf858977db9ba68ce7c
yang <gnon5367@naver.com>
2021-11-25 16:55:47 +0900

    step1

--- 05_igass.tf
+++ 05_igass.tf
@@ -1,11 +1,11 @@
-resource "aws_route_table_association" "yangsh_igas" {
-    subnet_id = [aws_subnet.yangsh_puba.id]
-    aws_route_table_id = aws_route_table.yangsh_rt.id
+resource "aws_route_table_association" "yangsh_puba" {
+    subnet_id = aws_subnet.yangsh_puba.id
+    route_table_id = aws_route_table.yangsh_rt.id
+
+}
+resource "aws_route_table_association" "yangsh_pubc" {
+    subnet_id = aws_subnet.yangsh_pubc.id
+    route_table_id = aws_route_table.yangsh_rt.id
 
 }
 
-resource "aws_route_table_association" "yangsh_igas" {
-    subnet_id = [aws_subnet.yangsh_pubc.id]
-    aws_route_table_id = aws_route_table.yangsh_rt.id
-    
-}
